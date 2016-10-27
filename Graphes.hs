294577628c9eaa9715508f4c7c345e6911581fdf
Echologie <Echologie@gmail.com>
2016-10-27 08:41:33 +0200

    Ajout de commentaire

--- Graphes.hs
+++ Graphes.hs
@@ -1,1 +1,5 @@
-{- Module visant à implémenter un type Graphe -}
+{- Module visant à implémenter un type Graphe. Un graphe doit
+   pouvoir décrire aussi bien une représentation graphique de
+   fonction que n'importe quelle construction géométrique. Il
+   doit aussi être possible de décrire des transformations af-
+   fines, projectives ou autres. -}
